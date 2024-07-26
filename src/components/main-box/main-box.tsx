be7e2c4a27c1e198d9c5940682c56777bbf63e5e
Mike Linder <miky2493@gmail.com>
2024-07-26 19:23:21 -0300

    implemented api request and streamlined information

--- src/components/main-box/main-box.tsx
+++ src/components/main-box/main-box.tsx
@@ -1,32 +1,55 @@
-import { useRef } from 'react';
 import './main-box.css';
-import { FormServices } from '../../services/form-services';
-import weatherImg from '../../utils/weather-img.png'
+import React, { FormEvent, useState } from 'react';
+import { ApiData, ApiResponse } from '../../services/apiData';
 
-export function MainBox() {
-    const formRef = useRef<HTMLFormElement>(null)
-    const inputRef = useRef<HTMLInputElement>(null)
+export const MainBox: React.FC = () => {
+    const [ inputValue, setInputValue ] = useState("")
+    const [ apiResponseData, setApiResponseData ] = useState({} as ApiResponse )
+    let [ submitedInputValue, setSubmitedInputValue ] = useState(false)
 
-
-    if (formRef && inputRef) {
-        FormServices(formRef.current, inputRef.current)
-    }
+    const FormSubmit = async (event: FormEvent) => {
+        event.preventDefault()
     
+        if ( inputValue.length < 3 ){
+            alert("A cidade deve ter um nome com pelo menos 3 caracteres.")
+            return
+        }
+
+        const apiResponse = await ApiData(inputValue)
+        
+        apiResponse && setApiResponseData(apiResponse)
+
+        setSubmitedInputValue(submitedInputValue = true)        
+    }
 
     return (
         <section id='main'>
             <h1 id='title'>CONDIÇÕES DO TEMPO</h1>
-            <form ref={formRef} id='form-content' action="submit">
-                <input ref={inputRef} id='input-form' type="text" placeholder='Digite o nome da cidade...'/>
-                <button>Pesquisar</button>
+            <form 
+                id='form-content' 
+                action="submit"
+                onSubmit={FormSubmit}
+            >
+                <input 
+                    id='input-form' 
+                    type="text" 
+                    placeholder='Digite o nome da cidade...'
+                    value={inputValue}
+                    onChange={(event) => setInputValue(event.target.value)}
+                />
+                <button 
+                    type='submit'
+                >Pesquisar</button>
             </form>
-            <section id="search-result">
-                <div id="loc-temp-content">
-                    <p id='loc'>Florianópolis</p>
-                    <p id='temp'>14ºC</p>
-                </div>
-                <img src={weatherImg} alt="imagem ilustrativa da condição do tempo" />
-            </section>
+            { submitedInputValue  &&             
+                <section id="search-result">
+                    <div id="loc-temp-content">
+                        <p id='loc'>{ apiResponseData.name }</p>
+                        <p id='temp'>{`${apiResponseData.temp}ºC`}</p>
+                    </div>
+                    <img src={ apiResponseData.weatherImg } alt="imagem ilustrativa da condição do tempo" />
+                </section>
+            }
         </section>
     )
 }
---
+++ src/services/apiData.ts
@@ -0,0 +1,28 @@
+export class ApiResponse {
+       name: string = ''
+       temp: string = ''
+       weatherImg: string = ''
+}
+
+export async function ApiData(cityName: string) {
+       const data = new ApiResponse
+
+       try {
+              const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=11949befd3b0d536fcce214312f4060e&lang=pt_br&units=metric`)
+
+              const database = await response.json()
+
+              data.name = database.name
+
+              data.temp = String(Math.round(database.main.temp))
+
+              const weatherImgObject = await fetch(`https://openweathermap.org/img/wn/${database.weather[0].icon}@2x.png`)
+
+              data.weatherImg = weatherImgObject.url              
+              
+              return data
+
+       } catch(err) {
+        console.log('Erro ao tentar conectar com a API: ', err);
+       }
+}
